d81b5c535295230d86d3c566cfed4d77d6e6bde9
symonogwe <symonogwe@gmail.com>
2023-05-15 10:36:49 +0300

    Add animation to the Modal

--- app.js
+++ app.js
@@ -11,10 +11,10 @@ openButton.addEventListener("click", function () {
 
 closeBtn.addEventListener("click", function () {
     modalContainer.style.display = "none";
-})
+});
 
 window.addEventListener("click", function (e) {
     if (e.target === modalContainer) {
         modalContainer.style.display = "none";
     }
-})
+});
