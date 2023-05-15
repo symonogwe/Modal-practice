5ac8d22da7f65601ef73bb9a4819dee989a73b0e
symonogwe <symonogwe@gmail.com>
2023-05-15 10:27:11 +0300

    Add event listeners to button, modal & window

---
+++ app.js
@@ -0,0 +1,20 @@
+
+// Variables
+const openButton = document.getElementById("open-btn");
+const modalContainer = document.getElementById("modal-container");
+const closeBtn = document.getElementById("close-btn");
+
+// Event Listeners
+openButton.addEventListener("click", function () {
+    modalContainer.style.display = "block";
+});
+
+closeBtn.addEventListener("click", function () {
+    modalContainer.style.display = "none";
+})
+
+window.addEventListener("click", function (e) {
+    if (e.target === modalContainer) {
+        modalContainer.style.display = "none";
+    }
+})
